9544e80fe45e12cf38c0aed1117357042e1f29d5
Serop Babikian <seropbabikian@Serops-MacBook-Pro.local>
2016-01-23 17:28:55 +0400

    Parsing and displaying all pokemons on the screen

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Serop Babikian on 1/23/16.
+//  Copyright © 2016 serop56.com. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Serop Babikian on 1/23/16.
+//  Copyright © 2016 serop56.com. All rights reserved.
+//
+
+import Foundation
